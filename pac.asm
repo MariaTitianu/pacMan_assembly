8e546b21637d9e97bc5c00149ba375f7fcfb99d1
Maria Titianu <maria.titianu.3004@gmail.com>
2022-05-19 18:51:18 +0300

    pac_canvas_2

--- pac.asm
+++ pac.asm
@@ -1,4 +1,4 @@
-.386
+.586
 .model flat, stdcall
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -7,6 +7,7 @@ includelib msvcrt.lib
 extern exit: proc
 extern malloc: proc
 extern memset: proc
+extern printf: proc
 
 includelib canvas.lib
 extern BeginDrawing: proc
@@ -19,7 +20,7 @@ public start
 ;sectiunile programului, date, respectiv cod
 .data
 ;aici declaram date
-window_title DB "Exemplu proiect desenare",0
+window_title DB "Pac-Man",0
 area_width EQU 640
 area_height EQU 480
 area DD 0
@@ -33,8 +34,16 @@ arg4 EQU 20
 
 symbol_width EQU 10
 symbol_height EQU 20
+
 include digits.inc
 include letters.inc
+
+buton_x equ 50
+buton_y equ 10
+buton_size equ 40
+
+error_iesire_matrice db "Ati incercat sa desenati un pixel pe afara: x = %d, y = %d, culoare = %x",13,10,0
+error_suprapunere_culoare_diferita db "Well...s-a pus o alta culoare: x = %d, y = %d, culoare = %x",13,10,0
 
 .code
 ; procedura make_text afiseaza o litera sau o cifra la coordonatele date
@@ -42,11 +51,74 @@ include letters.inc
 ; arg2 - pointer la vectorul de pixeli
 ; arg3 - pos_x
 ; arg4 - pos_y
+
+_make_pixel proc
+	push ebp
+	mov ebp, esp
+	;corp 
+	mov eax, [ebp+12]
+	mov ecx, [esp+8]
+	cmp ecx,area_width
+	jge it_s_not_ok_now
+	cmp ecx,0
+	jl it_s_not_ok_now
+	cmp eax,area_height
+	jge it_s_not_ok_now
+	cmp eax,0
+	jl it_s_not_ok_now
+	mov eax, [ebp+12]	;eax = y
+	mov ebx,area_width
+	mul ebx  ;eax = y* area_width
+	add eax,[ebp+8]	;eax= y* area_width+x
+	shl eax, 2;eax= (y* area_width+x)*4
+	add eax, area	
+	mov edi, [ebp+16]
+	cmp dword ptr[eax], 000000h
+	je culoare_neagra
+	pusha
+	push [ebp + 16]
+	push [ebp + 12]
+	push [ebp + 8]
+	push offset error_suprapunere_culoare_diferita
+	call printf
+	add esp,16
+	popa
+culoare_neagra:
+	mov dword ptr[eax], edi
+	jmp finish_make_pixel
+it_s_not_ok_now:
+	pusha
+	push [ebp + 16]
+	push [ebp + 12]
+	push [ebp + 8]
+	push offset error_iesire_matrice
+	call printf
+	add esp,16
+	popa
+finish_make_pixel:	
+	;endcorp
+	mov ESP, EBP
+	pop EBP
+	ret
+_make_pixel endp
+
+make_pixel macro x, y, culoare
+	pusha
+	mov esi, culoare
+	mov edi, y
+	mov ebx, x
+	push esi
+	push edi
+	push ebx
+	call _make_pixel
+	add esp, 12
+	popa
+endm
+
 make_text proc
 	push ebp
 	mov ebp, esp
 	pusha
-	
 	mov eax, [ebp+arg1] ; citim simbolul de afisat
 	cmp eax, 'A'
 	jl make_digit
@@ -91,7 +163,7 @@ bucla_simbol_coloane:
 	je simbol_pixel_alb
 	mov dword ptr [edi], 0
 	jmp simbol_pixel_next
-simbol_pixel_alb:
+simbol_pixel_alb:		
 	mov dword ptr [edi], 0FFFFFFh
 simbol_pixel_next:
 	inc esi
@@ -115,16 +187,129 @@ make_text_macro macro symbol, drawArea, x, y
 	add esp, 16
 endm
 
+_linie_orizontala proc 
+	push EBP
+	mov EBP, ESP 
+;corp_procedura	
+	mov ecx, [ebp + 16]
+	mov eax, [ebp + 8]
+bucla_linie:
+	make_pixel eax, [ebp+12], [ebp+20]
+	add eax, 1
+	loop bucla_linie
+;end corp
+	mov ESP, EBP
+	pop EBP
+	ret
+_linie_orizontala endp
+
+linie_orizontala macro x, y, len, culoare
+	pusha
+	mov edi, culoare
+	mov ebx, len
+	mov edx, y
+	mov eax, x
+	push edi 
+	push ebx
+	push edx
+	push eax
+	CALL _linie_orizontala
+	add esp, 16
+	popa
+endm
+
+_linie_verticala proc
+ 	push EBP
+	mov EBP, ESP 
+;corp_procedura	
+	mov ecx, [ebp + 16]
+	mov eax, [ebp+12]
+bucla_linie:
+	make_pixel [ebp + 8], eax, [ebp+20]
+	add eax, 1
+	loop bucla_linie
+;end corp
+	mov ESP, EBP
+	pop EBP
+	ret
+_linie_verticala endp
+
+linie_verticala macro x, y, len, culoare
+	pusha
+	mov edi, culoare
+	mov ebx, len
+	mov edx, y
+	mov eax, x
+	push edi 
+	push ebx
+	push edx
+	push eax
+	CALL _linie_verticala
+	add esp, 16
+	popa
+endm
+
+
+_Drept_gol  proc
+	push EBP
+	mov EBP, ESP 
+	;corp procedura
+
+	;end procedura
+	mov ESP, EBP
+	pop EBP
+	ret
+_Drept_gol endp
+
+drept_gol macro x, y,lungime,latime,gros,culoare
+	pusha
+	push culoare
+	push gros
+	push latime
+	push lungime
+	push y
+	push x
+	call _Drept_gol 
+	add esp, 24
+	popa
+endm
+
+_Drept_plin  proc
+	push EBP
+	mov EBP, ESP 
+	mov ecx, [ebp+20]
+	mov eax, [ebp+ 8]
+	;corp procedura
+looping:
+	linie_verticala eax, [ebp+12], [ebp+16],[ebp+24]
+	inc eax
+	loop looping 
+	;end procedura
+	mov ESP, EBP
+	pop EBP
+	ret
+_Drept_plin endp
+
+drept_plin macro x, y,lungime,latime,culoare
+	pusha
+	push culoare
+	push latime
+	push lungime
+	push y
+	push x
+	call _Drept_plin
+	add esp, 20
+	popa
+endm
 ; functia de desenare - se apeleaza la fiecare click
 ; sau la fiecare interval de 200ms in care nu s-a dat click
-; arg1 - evt (0 - initializare, 1 - click, 2 - s-a scurs intervalul fara click, 3 - s-a apasat o tasta)
-; arg2 - x (in cazul apasarii unei taste, x contine codul ascii al tastei care a fost apasata)
+; arg1 - evt (0 - initializare, 1 - click, 2 - s-a scurs intervalul fara click)
+; arg2 - x
 ; arg3 - y
 draw proc
 	push ebp
 	mov ebp, esp
 	pusha
-	
 	mov eax, [ebp+arg1]
 	cmp eax, 1
 	jz evt_click
@@ -136,34 +321,14 @@ draw proc
 	mul ebx
 	shl eax, 2
 	push eax
-	push 255
+	push 000
 	push area
 	call memset
 	add esp, 12
 	jmp afisare_litere
 	
 evt_click:
-	mov edi, area
-	mov ecx, area_height
-	mov ebx, [ebp+arg3]
-	and ebx, 7
-	inc ebx
-bucla_linii:
-	mov eax, [ebp+arg2]
-	and eax, 0FFh
-	; provide a new (random) color
-	mul eax
-	mul eax
-	add eax, ecx
-	push ecx
-	mov ecx, area_width
-bucla_coloane:
-	mov [edi], eax
-	add edi, 4
-	add eax, ebx
-	loop bucla_coloane
-	pop ecx
-	loop bucla_linii
+
 	jmp afisare_litere
 	
 evt_timer:
@@ -188,28 +353,6 @@ afisare_litere:
 	div ebx
 	add edx, '0'
 	make_text_macro edx, area, 10, 10
-	
-	;scriem un mesaj
-	make_text_macro 'P', area, 110, 100
-	make_text_macro 'R', area, 120, 100
-	make_text_macro 'O', area, 130, 100
-	make_text_macro 'I', area, 140, 100
-	make_text_macro 'E', area, 150, 100
-	make_text_macro 'C', area, 160, 100
-	make_text_macro 'T', area, 170, 100
-	
-	make_text_macro 'L', area, 130, 120
-	make_text_macro 'A', area, 140, 120
-	
-	make_text_macro 'A', area, 100, 140
-	make_text_macro 'S', area, 110, 140
-	make_text_macro 'A', area, 120, 140
-	make_text_macro 'M', area, 130, 140
-	make_text_macro 'B', area, 140, 140
-	make_text_macro 'L', area, 150, 140
-	make_text_macro 'A', area, 160, 140
-	make_text_macro 'R', area, 170, 140
-	make_text_macro 'E', area, 180, 140
 
 final_draw:
 	popa
@@ -217,6 +360,7 @@ final_draw:
 	pop ebp
 	ret
 draw endp
+
 
 start:
 	;alocam memorie pentru zona de desenat
@@ -228,7 +372,7 @@ start:
 	call malloc
 	add esp, 4
 	mov area, eax
-	;apelam functia de desenare a ferestrei
+	; apelam functia de desenare a ferestrei
 	; typedef void (*DrawFunc)(int evt, int x, int y);
 	; void __cdecl BeginDrawing(const char *title, int width, int height, unsigned int *area, DrawFunc draw);
 	push offset draw
@@ -238,8 +382,7 @@ start:
 	push offset window_title
 	call BeginDrawing
 	add esp, 20
-	
-	;terminarea programului
+	; terminarea programului
 	push 0
 	call exit
 end start
--- pac_man.asm
+++ pac_man.asm
@@ -1,4 +1,4 @@
-.386
+.586
 .model flat, stdcall
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -7,6 +7,7 @@ includelib msvcrt.lib
 extern exit: proc
 extern malloc: proc
 extern memset: proc
+extern printf: proc
 
 includelib canvas.lib
 extern BeginDrawing: proc
@@ -19,9 +20,9 @@ public start
 ;sectiunile programului, date, respectiv cod
 .data
 ;aici declaram date
-window_title DB "Exemplu proiect desenare",0
-area_width EQU 640
-area_height EQU 480
+window_title DB "Pac-Man",0
+area_width EQU 1920
+area_height EQU 1080
 area DD 0
 
 counter DD 0 ; numara evenimentele de tip timer
@@ -36,12 +37,82 @@ symbol_height EQU 20
 include digits.inc
 include letters.inc
 
+buton_x equ 50
+buton_y equ 10
+buton_size equ 40
+
+error_iesire_matrice db "Ati incercat sa desenati un pixel pe afara: x = %d, y = %d, culoare = %x",13,10,0
+error_suprapunere_culoare_diferita db "Well...s-a pus o alta culoare: x = %d, y = %d, culoare = %x",13,10,0
+
 .code
 ; procedura make_text afiseaza o litera sau o cifra la coordonatele date
 ; arg1 - simbolul de afisat (litera sau cifra)
 ; arg2 - pointer la vectorul de pixeli
 ; arg3 - pos_x
 ; arg4 - pos_y
+
+_make_pixel proc
+	push ebp
+	mov ebp, esp
+	;corp 
+	mov eax, [ebp+12]
+	mov ecx, [esp+8]
+	cmp ecx,area_width
+	jge it_s_not_ok_now
+	cmp ecx,0
+	jl it_s_not_ok_now
+	cmp eax,area_height
+	jge it_s_not_ok_now
+	cmp eax,0
+	jl it_s_not_ok_now
+	mov eax, [ebp+12]	;eax = y
+	mov ebx,area_width
+	mul ebx  ;eax = y* area_width
+	add eax,[ebp+8]	;eax= y* area_width+x
+	shl eax, 2;eax= (y* area_width+x)*4
+	add eax, area	
+	mov edi, [ebp+16]
+	cmp dword ptr[eax], 000000h
+	je culoare_neagra
+	pusha
+	push [ebp + 16]
+	push [ebp + 12]
+	push [ebp + 8]
+	push offset error_suprapunere_culoare_diferita
+	call printf
+	add esp,16
+	popa
+culoare_neagra:
+	mov dword ptr[eax], edi
+	jmp finish_make_pixel
+it_s_not_ok_now:
+	pusha
+	push [ebp + 16]
+	push [ebp + 12]
+	push [ebp + 8]
+	push offset error_iesire_matrice
+	call printf
+	add esp,16
+	popa
+finish_make_pixel:	
+	;endcorp
+	mov ESP, EBP
+	pop EBP
+	ret
+_make_pixel endp
+
+make_pixel macro x, y, culoare
+	pusha
+	mov esi, culoare
+	mov edi, y
+	mov ebx, x
+	push esi
+	push edi
+	push ebx
+	call _make_pixel
+	add esp, 12
+	popa
+endm
 make_text proc
 	push ebp
 	mov ebp, esp
@@ -88,11 +159,11 @@ bucla_simbol_linii:
 	mov ecx, symbol_width
 bucla_simbol_coloane:
 	cmp byte ptr [esi], 0
-	je simbol_pixel_alb
-	mov dword ptr [edi], 0
-	jmp simbol_pixel_next
-simbol_pixel_alb:
+	je simbol_pixel_negru
 	mov dword ptr [edi], 0FFFFFFh
+	jmp simbol_pixel_next
+simbol_pixel_negru:
+	mov dword ptr [edi], 0000000h
 simbol_pixel_next:
 	inc esi
 	add edi, 4
@@ -115,6 +186,120 @@ make_text_macro macro symbol, drawArea, x, y
 	add esp, 16
 endm
 
+_linie_orizontala proc 
+	push EBP
+	mov EBP, ESP 
+;corp_procedura	
+	mov ecx, [ebp + 16]
+	mov eax, [ebp + 8]
+bucla_linie:
+	make_pixel eax, [ebp+12], [ebp+20]
+	add eax, 1
+	loop bucla_linie
+;end corp
+	mov ESP, EBP
+	pop EBP
+	ret
+_linie_orizontala endp
+
+linie_orizontala macro x, y, len, culoare
+	pusha
+	mov edi, culoare
+	mov ebx, len
+	mov edx, y
+	mov eax, x
+	push edi 
+	push ebx
+	push edx
+	push eax
+	CALL _linie_orizontala
+	add esp, 16
+	popa
+endm
+
+_linie_verticala proc
+ 	push EBP
+	mov EBP, ESP 
+;corp_procedura	
+	mov ecx, [ebp + 16]
+	mov eax, [ebp+12]
+bucla_linie:
+	make_pixel [ebp + 8], eax, [ebp+20]
+	add eax, 1
+	loop bucla_linie
+;end corp
+	mov ESP, EBP
+	pop EBP
+	ret
+_linie_verticala endp
+
+linie_verticala macro x, y, len, culoare
+	pusha
+	mov edi, culoare
+	mov ebx, len
+	mov edx, y
+	mov eax, x
+	push edi 
+	push ebx
+	push edx
+	push eax
+	CALL _linie_verticala
+	add esp, 16
+	popa
+endm
+
+
+_Drept_gol  proc
+	push EBP
+	mov EBP, ESP 
+	;corp procedura
+
+	;end procedura
+	mov ESP, EBP
+	pop EBP
+	ret
+_Drept_gol endp
+
+drept_gol macro x, y,lungime,latime,gros,culoare
+	pusha
+	push culoare
+	push gros
+	push latime
+	push lungime
+	push y
+	push x
+	call _Drept_gol 
+	add esp, 24
+	popa
+endm
+
+_Drept_plin  proc
+	push EBP
+	mov EBP, ESP 
+	mov ecx, [ebp+20]
+	mov eax, [ebp+ 8]
+	;corp procedura
+looping:
+	linie_verticala eax, [ebp+12], [ebp+16],[ebp+24]
+	inc eax
+	loop looping 
+	;end procedura
+	mov ESP, EBP
+	pop EBP
+	ret
+_Drept_plin endp
+
+drept_plin macro x, y,lungime,latime,culoare
+	pusha
+	push culoare
+	push latime
+	push lungime
+	push y
+	push x
+	call _Drept_plin
+	add esp, 20
+	popa
+endm
 ; functia de desenare - se apeleaza la fiecare click
 ; sau la fiecare interval de 200ms in care nu s-a dat click
 ; arg1 - evt (0 - initializare, 1 - click, 2 - s-a scurs intervalul fara click, 3 - s-a apasat o tasta)
@@ -136,34 +321,14 @@ draw proc
 	mul ebx
 	shl eax, 2
 	push eax
-	push 255
+	push 0
 	push area
 	call memset
 	add esp, 12
 	jmp afisare_litere
 	
 evt_click:
-	mov edi, area
-	mov ecx, area_height
-	mov ebx, [ebp+arg3]
-	and ebx, 7
-	inc ebx
-bucla_linii:
-	mov eax, [ebp+arg2]
-	and eax, 0FFh
-	; provide a new (random) color
-	mul eax
-	mul eax
-	add eax, ecx
-	push ecx
-	mov ecx, area_width
-bucla_coloane:
-	mov [edi], eax
-	add edi, 4
-	add eax, ebx
-	loop bucla_coloane
-	pop ecx
-	loop bucla_linii
+	inc counter
 	jmp afisare_litere
 	
 evt_timer:
@@ -189,27 +354,7 @@ afisare_litere:
 	add edx, '0'
 	make_text_macro edx, area, 10, 10
 	
-	;scriem un mesaj
-	make_text_macro 'P', area, 110, 100
-	make_text_macro 'R', area, 120, 100
-	make_text_macro 'O', area, 130, 100
-	make_text_macro 'I', area, 140, 100
-	make_text_macro 'E', area, 150, 100
-	make_text_macro 'C', area, 160, 100
-	make_text_macro 'T', area, 170, 100
-	
-	make_text_macro 'L', area, 130, 120
-	make_text_macro 'A', area, 140, 120
-	
-	make_text_macro 'A', area, 100, 140
-	make_text_macro 'S', area, 110, 140
-	make_text_macro 'A', area, 120, 140
-	make_text_macro 'M', area, 130, 140
-	make_text_macro 'B', area, 140, 140
-	make_text_macro 'L', area, 150, 140
-	make_text_macro 'A', area, 160, 140
-	make_text_macro 'R', area, 170, 140
-	make_text_macro 'E', area, 180, 140
+
 
 final_draw:
 	popa
